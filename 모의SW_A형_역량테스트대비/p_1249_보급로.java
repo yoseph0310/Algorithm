c14f1d292a79f150a38e2fd4f19046f960afbedb
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-04-12 17:36:38 +0900

    SW 1249 보급로

---
+++ 모의SW_A형_역량테스트대비/p_1249_보급로.java
@@ -0,0 +1,14 @@
+package 모의SW_A형_역량테스트대비.p_1249;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class p_1249_보급로 {
+	
+	
+	public static void main(String[] args) throws Exception { 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+}
