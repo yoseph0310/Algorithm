7aafb01e489f8dfc6f81c1485b38d542ee586239
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2021-04-16 15:34:10 +0900

    BJ 1002 터렛 S4

---
+++ BJ_실버/p_1002_터렛.java
@@ -0,0 +1,68 @@
+package BJ_실버.p_1002_터렛;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p_1002_터렛 {
+	
+	static int T;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int r1 = Integer.parseInt(st.nextToken());
+			
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int r2 = Integer.parseInt(st.nextToken());
+			
+			System.out.println(point(x1,y1,r1,x2,y2,r2));
+		}
+		
+		
+	}
+	
+	static int point(int x1, int y1, int r1, int x2, int y2, int r2) {
+		int distance_pow = (int)(Math.pow(x2-x1, 2) + (Math.pow(y2-y1, 2)));
+		
+		// case 1 : 중점이 같으면서 반지름도 같을 경우
+		if ( x1 == x2 && y1 == y2 && r1 == r2) {
+			return -1;
+		}
+		
+		// case 2-1 : 두 원의 반지름 합보다 중점간 거리가 더 길 때
+		else if ( distance_pow > Math.pow(r1 + r2, 2)) {
+			return 0;
+		}
+		
+		// case 2-2 : 원 안에 원이 있으나 내접하지 않을 때
+		else if ( distance_pow < Math.pow(r2-r1, 2)) {
+			return 0;
+		}
+		
+		// case 3-1 : 내접
+		else if ( distance_pow == Math.pow(r2-r1, 2)) {
+			return 1;
+		}
+		
+		// case 3-2 : 외접
+		else if ( distance_pow == Math.pow(r1+r2, 2)) {
+			return 1;
+		}
+		
+		else {
+			return 2;
+		}
+
+	}
+
+}
